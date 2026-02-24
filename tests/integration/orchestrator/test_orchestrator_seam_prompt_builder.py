e412f2e07702c47c9a41053f7b130bd118b5bbb2
Yui Hao Goh <yuihaogoh@Yuis-MacBook-Air.local>
2026-02-24 23:12:36 +0800

    Add integration and seam test scaffolds

---
+++ tests/integration/orchestrator/test_orchestrator_seam_prompt_builder.py
@@ -0,0 +1,7 @@
+"""Seam integration scaffolds: orchestrator with real PromptBuilder."""
+
+
+def test_orchestrator_seam_prompt_builder_scaffold():
+    """TODO: wire orchestrator with real PromptBuilder and fakes for other components."""
+    assert True
+
